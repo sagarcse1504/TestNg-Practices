feb5cadb02ba1c68bebf837fc63a807975d1c020
Sagar <sagarcse1504@gmail.com>
2024-06-08 17:53:47 +0700

    First Class

---
+++ src/test/java/TestNgFirstclass/BaseMethodTest.java
@@ -0,0 +1,24 @@
+package TestNgFirstclass;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class BaseMethodTest {
+    WebDriver driver;
+    @BeforeMethod
+    public void setupBrowser(){
+        driver = new FirefoxDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+    }
+    @AfterMethod
+    public void closeBrowser() {
+        driver.quit();
+    }
+
+}
--- src/test/java/TestNgFirstclass/MethodAnnotationTest.java
+++ src/test/java/TestNgFirstclass/MethodAnnotationTest.java
@@ -8,65 +8,31 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class SwagLab {
+public class MethodAnnotationTest extends BaseMethodTest{
+
     @Test
 
     public void CheckTittle() {
-        WebDriver driver = new FirefoxDriver();
-        driver.get("https://www.saucedemo.com/");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-
         Assert.assertEquals(driver.getTitle(), "Swag Labs");
-        driver.quit();
-
     }
-
     @Test
     public void CheckUrl() {
-        WebDriver driver = new FirefoxDriver();
-        driver.get("https://www.saucedemo.com/");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/");
-        driver.quit();
-
     }
-
     @Test
     public void LoginFail() {
-        WebDriver driver = new FirefoxDriver();
-        driver.get("https://www.saucedemo.com/");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         driver.findElement(By.id("user-name")).sendKeys("adasfds");
         driver.findElement(By.id("login-button")).click();
-
         int count = driver.findElements(By.className("error-message-container")).size();
-
         Assert.assertEquals(count, 1);
-        driver.quit();
-
     }
-
     @Test
-
     public void LoginPass() {
-        WebDriver driver = new FirefoxDriver();
-        driver.get("https://www.saucedemo.com/");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
-
         driver.findElement(By.id("react-burger-menu-btn")).click();
         String Logout_text = driver.findElement(By.id("logout_sidebar_link")).getText().trim();
-
         Assert.assertEquals(Logout_text, "Logout");
-        driver.quit();
-
-
     }
 }
